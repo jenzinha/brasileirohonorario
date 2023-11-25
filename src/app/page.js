eff8d43de1b61893f678ed337ed90655bedb218e
jenzinha <101286269+jenzinha@users.noreply.github.com>
2023-11-24 22:20:54 -0300

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <body>
+    <header>
+      <div>
+        <h3> Lewis Hamilton</h3>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#">Página inicial</a> | </li>
+          <li><a href="#">Sobre</a> | </li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+    <section className="content">
+      <h1>Dia da Consciência Negra</h1>
+      <h2>20 de novembro</h2>
+      <div className="content-wrapper">
+        <p>
+          O Dia da Consciência Negra é celebrado em 20 de novembro no Brasil, em memória à morte de Zumbi dos Palmares, líder do Quilombo dos Palmares, um dos maiores quilombos do período colonial brasileiro.
+        </p>
+        <img src="https://img.freepik.com/vetores-gratis/dia-da-consciencia-negra-plana_52683-49076.jpg" alt="a" />
+      </div>
+      <h4>Lewis Hamilton</h4>
+      <h5>Piloto Formula 1</h5>
+      <div className="content-wrapper">
+      <img src="https://media.architecturaldigest.com/photos/646f85516e835723646f8e1b/16:9/w_2560%2Cc_limit/GettyImages-1252342308.jpg" alt="Lewis Hamilton" />
+      <p>
+          Lewis Hamilton é um piloto de Fórmula 1 inglês, considerado um dos maiores da história da categoria. O único negro a competir na Fórmula 1 e é também o piloto com mais títulos mundiais (sete).
+          É um ativista da igualdade racial e da diversidade no esporte, além de defensor da causa negra e usa sua plataforma para promover a igualdade e a justiça social.   
+          </p>
+      </div>
+      <h1>Biografia e Feitos</h1>
+      <div className="content-wrapper">
+        <p className="bio">
+        Lewis Carl Davidson Hamilton nasceu em Stevenage, Hertfordshire, Reino Unido, em 7 de janeiro de 1985. Filho de Anthony Hamilton e Carmen Larbalestier, Lewis teve um início modesto, mas seu talento excepcional se revelou cedo nas pistas de kart. Seu notável desempenho chamou a atenção de Ron Dennis, chefe da McLaren, que o contratou para fazer parte do programa de jovens pilotos da equipe. Esse foi o ponto de partida para uma jornada que o levaria a se tornar um dos pilotos mais bem-sucedidos da Fórmula 1.</p>
+        <img src="https://imgresizer.eurosport.com/unsafe/1200x0/filters:format(jpeg)/origin-imgresizer.eurosport.com/2020/11/15/2937217-60300308-2560-1440.jpg" alt="a" />
+      </div>
+      <p className="sec">
+      Hamilton fez sua estreia em 2007 pela McLaren e surpreendeu o mundo ao terminar em terceiro lugar em sua corrida de estreia no Grande Prêmio da Austrália. Sua temporada de estreia foi histórica, alcançando uma série de recordes, incluindo o recorde de mais pontos acumulados por um estreante e a primeira vitória em sua carreira no GP do Canadá.
+     </p>
+      <p className="sec">
+      O britânico rapidamente se estabeleceu como um concorrente formidável, conquistando seu primeiro título mundial em 2008, tornando-se o mais jovem campeão na época. Essa vitória marcou o início de uma era de domínio para Lewis, que acumularia múltiplos campeonatos ao longo de sua carreira. </p>
+      
+      <div className="content-wrapper">
+      <img className="a" src="https://s2.glbimg.com/OimSfcL2as-5vBUt6Zik4XJ_KzY=/smart/e.glbimg.com/og/ed/f/original/2020/12/26/lewis-hamilton-black-lives-matter-gq.jpg" alt="Lewis Hamilton" />
+      <p className="u">
+      Além de suas habilidades na pista, Hamilton também é conhecido por seu ativismo e envolvimento em causas sociais. Ele se tornou um defensor vocal da diversidade e inclusão no esporte automotivo, usando sua plataforma para promover mudanças positivas e lutar contra a injustiça social.        
+     </p>
+     </div>
+     <p className="sec">
+     Ao longo dos anos, Lewis Hamilton se destacou não apenas como um piloto excepcional, mas também como um ícone cultural e uma força motriz para a mudança. Sua dedicação ao esporte, combinada com sua influência fora das pistas, solidificou seu lugar como uma das figuras mais impactantes e respeitadas na Fórmula 1 e além.
+     </p>
+     <div class="card">
+        <p className="socorro">"Acredito que todos nós temos o poder de fazer a diferença." <cite>Lewis Hamilton</cite></p>
+    </div>  </section>
+    <footer>
+      <p className="footer">Jamili Amaral © 2023</p>
+    </footer>
+  </body>
+  );
+}
